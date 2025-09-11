4f2f29e5d15677de439c2252c8af45e3749a10a1
bedtimebird <michaelbirdcet@gmail.com>
2025-09-11 21:10:33 +0000

    edits: page + searchbar

---
+++ components/SearchBar.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import SearchBar from '../components/SearchBar'; // Import the component
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>KidActiva - Find Your Child's Next Adventure</title>
+        <meta name="description" content="Discover and book kids' activities in Vancouver" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main">
+        <h1 className="title">
+          Find Your Child's Next Adventure in Vancouver
+        </h1>
+
+        {/* This is where you render the component */}
+        <SearchBar />
+      </main>
+    </div>
+  );
+}
