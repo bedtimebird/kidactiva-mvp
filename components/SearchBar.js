7a1c891ea3e0bc43fe8aced18d61f20029151692
bedtimebird <michaelbirdcet@gmail.com>
2025-09-11 21:19:29 +0000

    edits: search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -8,14 +8,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>KidActiva - Find Your Child's Next Adventure</title>
-        <meta name="description" content="Discover and book kids' activities in Vancouver" />
+        <title>KidActiva - Find Your Childs Next Adventure</title>
+        <meta name="description" content="Discover and book kids activities in Vancouver" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
         <h1 className="title">
-          Find Your Child's Next Adventure in Vancouver
+          Find Your Childs Next Adventure in Vancouver
         </h1>
 
         {/* This is where you render the component */}
