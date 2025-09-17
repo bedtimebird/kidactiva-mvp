1007c284fd5d8a87e9857d791b67d70f0da6a891
bedtimebird <michaelbirdcet@gmail.com>
2025-09-17 19:50:02 +0000

    edit: correct erros

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, [ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import ActivityCard from '../components/ActivityCard';
 
